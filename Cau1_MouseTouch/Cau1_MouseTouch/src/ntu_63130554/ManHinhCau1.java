773236395e87540833e79a56a9c9095c7af745f0
KhaiTran2003 <145674119+KhaiTran2003@users.noreply.github.com>
2024-03-25 12:19:02 +0700

    Cau1_DebugLoadAnh_COmplete

--- Cau1_MouseTouch/Cau1_MouseTouch/src/ntu_63130554/ManHinhCau1.java
+++ Cau1_MouseTouch/Cau1_MouseTouch/src/ntu_63130554/ManHinhCau1.java
@@ -1,47 +1,33 @@
 package ntu_63130554;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
 import java.io.IOException;
 import java.net.URL;
-import java.util.Random;
 
 public class ManHinhCau1 extends JFrame {
-
-    private JLabel[][] mouseLabels; 
-    private int numRows = 4; 
-    private int numCols = 5; 
-    private int labelWidth = 150; 
-    private int labelHeight = 150;
+    private JLabel mouseLabel;
 
     public ManHinhCau1() {
         setTitle("Catch the Mouse");
         setSize(800, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        mouseLabels = new JLabel[numRows][numCols];
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                JLabel mouseLabel = new JLabel();
-                URL urlIconMouse = ManHinhCau1.class.getResource("icon_mouse.jpg");
-                ImageIcon mouseIcon = createResizedImageIcon(urlIconMouse, labelWidth, labelHeight);
-                if (mouseIcon != null) {
-                    mouseLabel.setIcon(mouseIcon);
-                } else {
-                    System.err.println("Lỗi");
-                }
-                mouseLabel.setBounds(j * labelWidth, i * labelHeight, labelWidth, labelHeight);
-                getContentPane().add(mouseLabel);
-                mouseLabels[i][j] = mouseLabel;
-            }
-        }
-        
-        getContentPane().setLayout(null);
-    }
 
+        mouseLabel = new JLabel();
+        URL urlIconMouse = ManHinhCau1.class.getResource("icon_mouse.jpg");
+        ImageIcon mouseIcon = createResizedImageIcon(urlIconMouse, 50, 50); // Sử dụng phương thức createResizedImageIcon để tạo hình ảnh có kích thước đã thay đổi
+        if (mouseIcon != null) {
+            mouseLabel.setIcon(mouseIcon);
+        } else {
+            System.err.println("Couldn't load icon_mouse.jpg");
+        }
+
+        getContentPane().add(mouseLabel);
+    }
     protected ImageIcon createResizedImageIcon(URL url, int width, int height) {
         if (url != null) {
             try {
@@ -53,7 +39,7 @@ public class ManHinhCau1 extends JFrame {
                 return null;
             }
         } else {
-            System.err.println("Không tìm thấy: " + url);
+            System.err.println("Couldn't find file: " + url);
             return null;
         }
     }
