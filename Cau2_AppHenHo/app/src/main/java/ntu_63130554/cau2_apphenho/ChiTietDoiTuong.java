8e7b055795e0a298b8cb1eaac741cb4b72fde0e0
KhaiTran2003 <145674119+KhaiTran2003@users.noreply.github.com>
2024-03-25 09:58:11 +0700

    Cau2_AddActivity_ChiTietDoiTuong_ChuaCode

---
+++ Cau2_AppHenHo/app/src/main/java/ntu_63130554/cau2_apphenho/ChiTietDoiTuong.java
@@ -0,0 +1,14 @@
+package ntu_63130554.cau2_apphenho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChiTietDoiTuong extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chi_tiet_doi_tuong);
+    }
+}
