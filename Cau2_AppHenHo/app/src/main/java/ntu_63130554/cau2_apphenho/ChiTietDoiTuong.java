65ac3fd531ee4fde8e6da9912f4c0bfdffbf5507
KhaiTran2003 <145674119+KhaiTran2003@users.noreply.github.com>
2024-03-26 09:55:34 +0700

    Update_Cau2_XoaNham

---
+++ Cau2_AppHenHo/app/src/main/java/ntu_63130554/cau2_apphenho/ChiTietDoiTuong.java
@@ -0,0 +1,18 @@
+package ntu_63130554.cau2_apphenho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class ChiTietDoiTuong extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chi_tiet_doi_tuong);
+
+    }
+}
---
+++ Cau2_AppHenHo/app/src/main/java/ntu_63130554/cau2_apphenho/DoiTuong.java
@@ -0,0 +1,69 @@
+package ntu_63130554.cau2_apphenho;
+
+import java.io.Serializable;
+
+public class DoiTuong implements Serializable {
+    private String ho;
+    private String ten;
+    private String sdt;
+    private String diaChi;
+    private String gioiThieu;
+    private int idAvatar;
+
+    public DoiTuong(String ho, String ten, String sdt, String diaChi, String gioiThieu, int idAvatar) {
+        this.ho = ho;
+        this.ten = ten;
+        this.sdt = sdt;
+        this.diaChi = diaChi;
+        this.gioiThieu = gioiThieu;
+        this.idAvatar = idAvatar;
+    }
+
+    public String getHo() {
+        return ho;
+    }
+
+    public void setHo(String ho) {
+        this.ho = ho;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getGioiThieu() {
+        return gioiThieu;
+    }
+
+    public void setGioiThieu(String gioiThieu) {
+        this.gioiThieu = gioiThieu;
+    }
+
+    public int getIdAvatar() {
+        return idAvatar;
+    }
+
+    public void setIdAvatar(int idAvatar) {
+        this.idAvatar = idAvatar;
+    }
+}
