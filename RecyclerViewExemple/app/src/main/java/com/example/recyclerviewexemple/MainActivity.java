230ac0c5358e1e8c7c12cedc710a738b5c74f69c
KhaiTran2003 <145674119+KhaiTran2003@users.noreply.github.com>
2024-04-09 14:10:55 +0700

    Buoc3_getDataResource_and_Running

--- RecyclerViewExemple/app/src/main/java/com/example/recyclerviewexemple/MainActivity.java
+++ RecyclerViewExemple/app/src/main/java/com/example/recyclerviewexemple/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.recyclerviewexemple;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recyclerViewDatas;
+    RecyclerView recyclerViewLandscape;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewDatas = getDataForRecyclerView();
+        recyclerViewLandscape = findViewById(R.id.recyclerLand);
+        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
+        recyclerViewLandscape.setLayoutManager(layoutLinear);
+        landScapeAdapter = new LandScapeAdapter(this, recyclerViewDatas);
+        recyclerViewLandscape.setAdapter(landScapeAdapter);
+    }
+    ArrayList<LandScape> getDataForRecyclerView(){
+        ArrayList<LandScape> dsDuLieu = new ArrayList<>();
+        LandScape landScape1 = new LandScape("img","Ngôi nhà 1");
+        dsDuLieu.add(landScape1);
+        dsDuLieu.add(new LandScape("img_1","Ngôi nhà 2"));
+        dsDuLieu.add(new LandScape("img_2","Ngôi nhà 3"));
+        dsDuLieu.add(new LandScape("img_3","Ngôi nhà 4"));
+        dsDuLieu.add(new LandScape("img_4","Ngôi nhà 5"));
+        return dsDuLieu;
     }
 }
