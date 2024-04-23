10d78886ffb97dbbfa7b2bd9a230316429bd918e
KhaiTran2003 <145674119+KhaiTran2003@users.noreply.github.com>
2024-04-23 15:05:24 +0700

    Intent_NhapLieu_Buoc4_onActivityResult

--- Intent_ViDu2_NhapLieu/app/src/main/java/com/example/intent_vidu2_nhaplieu/MainActivity.java
+++ Intent_ViDu2_NhapLieu/app/src/main/java/com/example/intent_vidu2_nhaplieu/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,23 @@ public class MainActivity extends AppCompatActivity {
     }
     public void ChuyenManHinhNhapLieu(View v){
         Intent iChuyenManHinhNhapLieu = new Intent(this, NhapLieu.class);
-        startActivity(iChuyenManHinhNhapLieu);
+        startActivityForResult(iChuyenManHinhNhapLieu,500);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode==500)
+            if(resultCode==RESULT_OK){
+                String hoTen = data.getStringExtra("FN");
+                String sinhVienTruong = data.getStringExtra("SV");
+                TextView tvName = (TextView) findViewById(R.id.edt_hoten);
+                TextView tvSinhVien  =(TextView) findViewById(R.id.edt_sinhvientruong);
+                tvName.setText(hoTen);
+                tvSinhVien.setText(sinhVienTruong);
+            }
+            else
+                Toast.makeText(this,"Trả về thất bại",Toast.LENGTH_LONG);
+        else
+            super.onActivityResult(requestCode,resultCode,data);
+
     }
 }
