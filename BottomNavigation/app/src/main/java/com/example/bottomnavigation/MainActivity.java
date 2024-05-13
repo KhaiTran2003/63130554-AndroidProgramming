875788cfcf6758c2920958ce60321c5d2df105a0
KhaiTran2003 <145674119+KhaiTran2003@users.noreply.github.com>
2024-05-13 15:29:23 +0700

    bottomnavigation3

--- BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.java
+++ BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.java
@@ -3,11 +3,21 @@ package com.example.bottomnavigation;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.bottomnavigation.databinding.ActivityMainBinding;
+
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            return true;
+        });
     }
 }
