36c19e5eb5ccd6ba749a8fe7511038540216ed95
KhaiTran2003 <145674119+KhaiTran2003@users.noreply.github.com>
2024-05-26 14:49:26 +0700

    pheptinh2

--- OnTap_PhepToan/app/src/main/java/ntu/khai/ontap_pheptoan/MainActivity.java
+++ OnTap_PhepToan/app/src/main/java/ntu/khai/ontap_pheptoan/MainActivity.java
@@ -3,12 +3,70 @@ package ntu.khai.ontap_pheptoan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtSoA;
+    EditText edtSoB;
+    EditText edtKQ;
+    Button nutCong,nutTru,nutNhan,nutChia;
+
+    void TimDieuKhien(){
+        edtSoA = (EditText) (findViewById(R.id.id_edt_soA));
+        edtSoB = (EditText) (findViewById(R.id.id_edt_soB));
+        edtKQ = (EditText) findViewById(R.id.id_edtKetQua);
+        nutCong = (Button) findViewById(R.id.id_btnCong);
+        nutTru = (Button) findViewById(R.id.id_btnTru);
+        nutNhan = (Button) findViewById(R.id.id_btnNhan);
+        nutChia = (Button) findViewById(R.id.id_btnChia);
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TimDieuKhien();
+
     }
+
+    public void XuLyCong(View v){
+        String soA = edtSoA.getText().toString();
+        String soB = edtSoB.getText().toString();
+        Float so1 = Float.parseFloat(soA);
+        Float so2 = Float.parseFloat(soB);
+        float kq = so1 + so2;
+        String chuoiKQ = String.valueOf(kq);
+        edtKQ.setText(chuoiKQ);
+    }
+    public void XuLyTru(View v){
+        String so1 = edtSoA.getText().toString();
+        String so2 = edtSoB.getText().toString();
+        Float soA = Float.parseFloat(so1);
+        Float soB = Float.parseFloat(so2);
+        Float kq = soA - soB;
+        String chuoiKQ = String.valueOf(kq);
+        edtKQ.setText(chuoiKQ);
+    }
+    public void XuLyNhan(View v){
+        String so1 = edtSoA.getText().toString();
+        String so2 = edtSoB.getText().toString();
+        Float soA = Float.parseFloat(so1);
+        Float soB = Float.parseFloat(so2);
+        Float kq = soA * soB;
+        String chuoiKQ = String.valueOf(kq);
+        edtKQ.setText(chuoiKQ);
+    }
+    public void XuLyChia(View v){
+        String so1 = edtSoA.getText().toString();
+        String so2 = edtSoB.getText().toString();
+        Float soA = Float.parseFloat(so1);
+        Float soB = Float.parseFloat(so2);
+        Float kq = soA / soB;
+        String chuoiKQ = String.valueOf(kq);
+        edtKQ.setText(chuoiKQ);
+    }
+
 }
