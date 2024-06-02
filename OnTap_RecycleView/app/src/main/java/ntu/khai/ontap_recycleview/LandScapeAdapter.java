2b856cedb9634b28c1aa7a33427b51f9118cc296
KhaiTran2003 <145674119+KhaiTran2003@users.noreply.github.com>
2024-06-02 22:02:46 +0700

    recycleview_complete

---
+++ OnTap_RecycleView/app/src/main/java/ntu/khai/ontap_recycleview/LandScapeAdapter.java
@@ -0,0 +1,64 @@
+package ntu.khai.ontap_recycleview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
+public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.ItemLandHolder> {
+    Context context;
+    ArrayList<LandScape> lstData;
+
+    public LandScapeAdapter(Context context, ArrayList<LandScape> lstData) {
+        this.context = context;
+        this.lstData = lstData;
+    }
+
+    @NonNull
+    @Override
+    public ItemLandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater cai_bom = LayoutInflater.from(context);//bơm context vào
+        View vItem = cai_bom.inflate(R.layout.item_land, parent, false);
+        ItemLandHolder viewHolderCreated = new ItemLandHolder(vItem);
+        return viewHolderCreated;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemLandHolder holder, int position) {
+        //lấy ĐT hiển thị
+        LandScape landScapeHienThi = lstData.get(position);
+        //Trích thông tin
+        String caption = landScapeHienThi.getLandCation();
+        String tenFileAnh = landScapeHienThi.getLandImageFileName();
+        // đặt vào các trường thng tin holder
+        holder.tvCaption.setText(caption);
+        //dat ảnh
+        String packageName = holder.itemView.getContext().getPackageName();
+        int imageID = holder.itemView.getResources().getIdentifier(tenFileAnh,"mipmap",packageName);
+        holder.ivLandScape.setImageResource(imageID);
+    }
+
+    @Override
+    public int getItemCount() {
+        return lstData.size();
+    }
+
+    class ItemLandHolder extends RecyclerView.ViewHolder{
+        TextView tvCaption;
+        ImageView ivLandScape;
+
+        public ItemLandHolder(@NonNull View itemView) {
+            super(itemView);
+            tvCaption = itemView.findViewById(R.id.id_tvCaption);
+            ivLandScape = itemView.findViewById(R.id.id_imgAvarta);
+        }
+    }
+}
--- OnTap_RecycleView/app/src/main/java/ntu/khai/ontap_recycleview/MainActivity.java
+++ OnTap_RecycleView/app/src/main/java/ntu/khai/ontap_recycleview/MainActivity.java
@@ -1,14 +1,37 @@
 package ntu.khai.ontap_recycleview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recyleViewData;
+    RecyclerView recyclerViewLandScape;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyleViewData = getDataForRecycleView();
+        recyclerViewLandScape = findViewById(R.id.id_recyleview);
+        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
+        recyclerViewLandScape.setLayoutManager(layoutLinear);
+        landScapeAdapter = new LandScapeAdapter(this,recyleViewData);
+        recyclerViewLandScape.setAdapter(landScapeAdapter);
+    }
+    ArrayList<LandScape> getDataForRecycleView(){
+        ArrayList<LandScape> dsDuLieu = new ArrayList<LandScape>();
+        LandScape lc1 = new LandScape("avarta","Its me");
+        dsDuLieu.add(lc1);
+        dsDuLieu.add(new LandScape("dulich1","du lịch"));
+        dsDuLieu.add(new LandScape("dulich2","du lịch"));
+        dsDuLieu.add(new LandScape("tinhtoan1","tinhtoan1"));
+        dsDuLieu.add(new LandScape("tinhtoan2","dtinhtoan2"));
+        return dsDuLieu;
     }
 }
