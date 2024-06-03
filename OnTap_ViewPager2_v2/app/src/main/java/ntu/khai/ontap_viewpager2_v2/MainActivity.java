1c188aff76432ecc907b6b8e64f30cbb5749aefc
KhaiTran2003 <145674119+KhaiTran2003@users.noreply.github.com>
2024-06-03 07:36:11 +0700

    viewpager2_complete

--- OnTap_ViewPager2_v2/app/src/main/java/ntu/khai/ontap_viewpager2_v2/MainActivity.java
+++ OnTap_ViewPager2_v2/app/src/main/java/ntu/khai/ontap_viewpager2_v2/MainActivity.java
@@ -1,14 +1,41 @@
 package ntu.khai.ontap_viewpager2_v2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recyleViewData;
+    RecyclerView recyclerViewLandScape;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyleViewData = getDataForRecycleView();
+        recyclerViewLandScape = findViewById(R.id.id_recycleview);
+        //dọc
+//        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
+//        recyclerViewLandScape.setLayoutManager(layoutLinear);
+        //ngang
+        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewLandScape.setLayoutManager(layoutLinear);
+
+        landScapeAdapter = new LandScapeAdapter(this,recyleViewData);
+        recyclerViewLandScape.setAdapter(landScapeAdapter);
+    }
+    ArrayList<LandScape> getDataForRecycleView(){
+        ArrayList<LandScape> dsDuLieu = new ArrayList<LandScape>();
+        dsDuLieu.add(new LandScape("avarta","du lịch"));
+        dsDuLieu.add(new LandScape("dulich1","du lịch"));
+        dsDuLieu.add(new LandScape("tinhtoan1","tinhtoan1"));
+        dsDuLieu.add(new LandScape("tinhtoan2","dtinhtoan2"));
+        return dsDuLieu;
     }
 }
